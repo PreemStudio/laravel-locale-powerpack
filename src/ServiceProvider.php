562183e343c4611181b2af0236b8e248fab01995
Brian Faust <hello@basecode.sh>
2023-04-26 04:06:03 +0300

    first commit
    Signed-off-by: Brian Faust <hello@basecode.sh>

---
+++ src/ServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BombenProdukt\LocalePowerPack;
+
+use BombenProdukt\PackagePowerPack\Package\AbstractServiceProvider;
+
+final class ServiceProvider extends AbstractServiceProvider
+{
+    //
+}
